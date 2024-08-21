15e762d2e544c739c3d2d6618664a89c2f270268
SeokHoon <sughoon1@gmail.com>
2024-08-21 16:12:58 +0900

    11

---
+++ src/component/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUser} from "@fortawesome/free-regular-svg-icons";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+    const menuList = ['여성','남성','신생아/유아','아동','H&M Home','Sale','지속가능성'];
+    return (
+        <div>
+            <div>
+                <div className={"login-button"}>
+                    <FontAwesomeIcon icon={faUser}/>
+                    <div>로그인</div>
+                </div>
+            </div>
+            <div className={"nav-section"}>
+                <img width={100}
+                     src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSedfDVsziektqKloi6Btgik9aT6upswW0mfw&s"}/>
+            </div>
+            <div className={"menu-area"}>
+                <ul className={"menu-list"}>
+                    {menuList.map(menu => <li>{menu}</li>)}
+                </ul>
+                <div className={"search-area"}>
+                    <FontAwesomeIcon icon={faSearch}/>
+                    <input className={"search-input"}/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProductCard = () => {
+    return (
+        <div>
+            <img src={"https://noona-hnm.netlify.app/pattern-jacket.jpeg"}/>
+            <div>Conscious choose</div>
+            <div>벨티드 트윌 코드</div>
+            <div>9900</div>
+            <div>신제품</div>
+        </div>
+    );
+};
+
+export default ProductCard;
